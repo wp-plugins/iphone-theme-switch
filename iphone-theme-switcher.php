8719cc31bc32448d8e2a1c0913819d540ddf5f0c
JonasVorwerk <JonasVorwerk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-14 10:48:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/iphone-theme-switch/tags/0.5@263436 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iphone-theme-switcher.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: iPhone theme switcher
+Plugin URI: http://wordpress.org/extend/plugins/iphone-theme-switch/
+Description: This plugin detects if your site is being viewed by iPhone (or iPod) and switches to an selected iPhone theme.
+Version: 0.41
+Author: Jonas Vorwerk
+Author URI: http://www.jonasvorwerk.com/
+*/
+
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') !== FALSE || strpos($_SERVER['HTTP_USER_AGENT'], 'iPod') !== FALSE){ 
+	add_filter('stylesheet', 'getTemplateStyle');
+	add_filter('template', 'getTemplateStyle');
+} 
+
+function getTemplateStyle(){
+	$iphonetheme =  get_option('iphonetheme');
+    $themes = get_themes();
+	foreach ($themes as $theme_data) {
+	  if ($theme_data['Name'] == $iphonetheme) {
+	      return $theme_data['Stylesheet'];
+	  }
+	}	
+}
+
+function its_admin_actions() { 
+	if (!current_user_can('manage_options'))  {
+    	wp_die( __('You do not have sufficient permissions to access this page.') );
+	} else {
+		add_theme_page("iPhone theme switcher", "iPhone theme switcher", 'manage_options', "iPhone-theme-switcher", "its_show_admin");
+	}
+} 
+
+function its_show_admin(){
+	include('iphone-theme-switcher_admin.php'); 
+}
+
+add_action('admin_menu', 'its_admin_actions'); 
+
+?>
